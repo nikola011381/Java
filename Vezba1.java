edd8989abf3bdc5dd8429fcb1175a9b4910e75a4
Radovic <nikolaradovic381@gmail.com>
2023-02-26 14:10:34 +0100

    first commit

---
+++ Vezba1.java
@@ -0,0 +1,24 @@
+public class Vezba1 {
+    public static void main(String[] args) {
+/**
+ *
+ */
+        System.out.println("Hello world !!"); // komentar
+
+        /*
+         komentar
+        */
+
+        /**
+         *
+         */
+
+        System.out.println(2 + 2);
+        ispisiText("cao cao");
+    }
+
+    public static void ispisiText (String text  ) {
+        System.out.println(text);
+    }
+
+}
