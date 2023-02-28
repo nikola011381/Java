df8a5d4e29f3cc498845cc05241c2e865c85a7d7
Radovic <nikolaradovic381@gmail.com>
2023-02-28 12:47:26 +0100

    Vezba 2 commit

--- Vezba2.java
+++ Vezba2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Vezba2 {
 
     public static void main(String[] args) {
@@ -5,6 +7,12 @@ public class Vezba2 {
         int operand2=3;
         int sum=operand1 + operand2;
         System.out.println(sum);
-      
+
+        System.out.println("Unesi svoje ime:");
+
+        Scanner scanner = new Scanner(System.in);
+        String ime = scanner.nextLine();
+        System.out.println("Dobrodosao " + ime  );
+
     }
 }
