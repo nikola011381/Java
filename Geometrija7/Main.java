a1b4757833c0da2db5f89362779d219fd78c8042
Radovic <nikolaradovic381@gmail.com>
2023-03-01 21:00:28 +0100

    Vezba 7

--- Geometrija7/Main.java
+++ Geometrija7/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println(s.name +" cija je stranica dimenzije "+ s.x + " boja "+ s.color + " povrsina je "+ s.x*s.x);
 
         Circle k =new Circle("Krug", "bela", 6);
-        Rectangle r =new Rectangle("Pravugaonik ", "crveni ", 12);
+        Rectangle r =new Rectangle("Pravugaonik ", "crveni ", 11);
 
     }
 }
